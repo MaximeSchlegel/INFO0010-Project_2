f5a1a221a61d7862b5c82440a98fb0c04dee143e
MaximeSchlegel <maxime.schlegel.83@gmail.com>
2019-05-12 18:52:26 +0200

    Clean Up cookie manager

--- src/CookieManager.java
+++ src/CookieManager.java
@@ -1,5 +1,6 @@
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import javafx.util.Pair;
-import java.io.*;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -7,50 +8,51 @@ import java.util.Random;
 
 
 public class CookieManager {
-    /*Handle the loading and saving of game linked to a cookie id
-    * Generate the cookie id and new game*/
-    private Map<String, BatThomi> saved_game;
-    private String cookieCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789"; //length = 62
-
+    private Map<String, BatThomi> savedGames;
+    private static String ALLOWED_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "0123456789"; //length = 62
+    private static int COOKIE_LENGTH = 16;
+    private static int[] SHIP_SIZE = {2,3,3,4,5};
 
     public CookieManager() {
-        this.saved_game = new HashMap<>();
+        this.savedGames = new HashMap<>();
     }
 
     public boolean isUsed(String id) {
-        return this.saved_game.containsKey(id);
+        return this.savedGames.containsKey(id);
     }
 
     public Pair<String,BatThomi> getNewGame() {
         Random rand = new Random();
-        String id = "";
-        for (int i = 0; i < 26; i++)
-            id += cookieCharacters.charAt(rand.nextInt(cookieCharacters.length()));
-        while(this.isUsed(id)) {
-            id = "";
-            for (int i = 0; i < 26; i++)
-                id += cookieCharacters.charAt(rand.nextInt(cookieCharacters.length()));
-        }
+        StringBuilder idBuilder;
+        String id;
 
-        int [] boats = {2,3,3,4,5};
-        System.out.println("Got this far");
-        BatThomi newGame = new BatThomi(boats);
-        this.saved_game.put(id, newGame);
+        //generate an used id
+        do {
+            idBuilder = new StringBuilder();
+            for (int i = 0; i < COOKIE_LENGTH; i++) {
+                idBuilder.append(ALLOWED_CHARACTERS.charAt(rand.nextInt(ALLOWED_CHARACTERS.length())));
+            }
+            id = idBuilder.toString();
+        } while(this.isUsed(id));
+
+        //create a new game
+        BatThomi newGame = new BatThomi(SHIP_SIZE);
+
+        this.savedGames.put(id, newGame);
         return new Pair<>(id,newGame);
     }
 
     public BatThomi getGame(String id){
-        if(this.isUsed(id))
-            return this.saved_game.get(id);
+        if(this.isUsed(id)) {
+            return this.savedGames.get(id);
+        }
         return null;
     }
 
-    public BatThomi loadGame(String id) {
-        return this.saved_game.get(id);
-    }
-
-    public void delete(String id) {
-        this.saved_game.remove(id);
+    public void deleteGame(String id) {
+        if(this.isUsed(id)){
+            this.savedGames.remove(id);
+        }
     }
 
 }
